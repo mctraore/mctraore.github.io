0c6d97da0c1888d17b89c612fbb80b0fa60aa735
Marie-Claire <mctraore@umd.edu>
2018-08-17 22:15:37 -0400

    php file update

--- contact.php
+++ contact.php
@@ -1,6 +1,15 @@
-<html>
-<body>
-Hi <?php echo htmlspecialchars($_POST['name']); ?>.
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $from = $_POST['from']
+    $message = $_POST['message'];
     
-</body>
-</html>
+    $send = "mctraore@icloud.com";
+    $headers = "From: ".$from;
+    $txt = "You have received an email from ".$from." .\n\n".$message;
+    
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+    
+}
